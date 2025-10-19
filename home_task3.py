9ce11026032b5b1bf9c5f63e120d1c9e52036e86
vhranovsky <vhranovskiy84@gmail.com>
2025-10-19 21:20:40 +0300

    fix function "print_directroy" in home_task3.py

--- home_task3.py
+++ home_task3.py
@@ -2,9 +2,12 @@ from colorama import Fore
 import os
 import sys
 
-directory_path = sys.argv[1] if len(sys.argv)>=2 else "D:/Education/python/goit-pycore-hw-04/"
+directory_path = sys.argv[1] if len(sys.argv)>=2 else "D:/Education/python/goit-pycore-hw-04"
 
 def print_directroy(path_to_dir:str,tab_count:int):
+    if path_to_dir[-1] != "/" and path_to_dir[-1] != "\\":
+        path_to_dir += "/" 
+
     try:
         all_entries = os.listdir(path_to_dir)
         
